07827d805c112ad9e7cb413b4eb5ba0f3594953e
Sam Lai <saint00771155@gmail.com>
2024-08-23 23:44:09 +0800

    feat: Update Package

--- Package.swift
+++ Package.swift
@@ -1,21 +1,26 @@
-// swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "LoadingView",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v11),
+        .tvOS(.v14),
+        .watchOS(.v7)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "LoadingView",
             targets: ["LoadingView"]),
     ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "LoadingView"),
-
+            name: "LoadingView",
+            dependencies: []),
+        .testTarget(
+            name: "LoadingViewTests",
+            dependencies: ["LoadingView"]),
     ]
 )
