b330a227270cdc707e6f6cd387840f9b36f7bf86
Sam Lai <saint00771155@gmail.com>
2024-08-24 16:02:33 +0800

    fix: Update modifier

--- Package.swift
+++ Package.swift
@@ -19,8 +19,5 @@ let package = Package(
         .target(
             name: "LoadingView",
             dependencies: []),
-        .testTarget(
-            name: "LoadingViewTests",
-            dependencies: ["LoadingView"]),
     ]
 )
