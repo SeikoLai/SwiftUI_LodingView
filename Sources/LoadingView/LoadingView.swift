34b06aa0d538a81dabdbb19a8d89b5b4f7f20148
Sam Lai <saint00771155@gmail.com>
2024-08-24 18:06:51 +0800

    feat: Expanded square size

--- Sources/LoadingView/LoadingView.swift
+++ Sources/LoadingView/LoadingView.swift
@@ -79,7 +79,7 @@ public struct LoadingView: View {
                 GeometryReader { geometry in
                     Color.clear
                         .onAppear {
-                            dimension = max(geometry.size.width, geometry.size.height) * 1.2
+                            dimension = max(geometry.size.width, geometry.size.height) * 1.5
                         }
                 }
             )
